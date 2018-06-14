b41f7f6ddc9fa8382b2a20320e4f6675104fb07a
Anirudh <icyph0x@pm.me>
2018-06-14 09:59:02 +0530

    feat: star all procs, add yellow

--- hue.nim
+++ hue.nim
@@ -1,14 +1,18 @@
-import strformat
-
-proc blue(s: string): string =
+# colors
+proc blue*(s: string): string =
   result = "\e[34m" & s & "\e[0m"
 
-proc bold(s: string): string =
+proc yellow*(s: string): string =
+  result = "\e[33m" & s & "\e[0m"
+
+# styles
+proc bold*(s: string): string =
   result = "\e[1m" & s & "\e[0m"
 
-proc italic(s: string): string =
+proc italic*(s: string): string =
   result = "\e[3m" & s & "\e[0m"
 
-proc que(s: string): string =
+# labels
+proc que*(s: string): string =
   result = "\e[33;1m[?] \e[0m" & s
 
