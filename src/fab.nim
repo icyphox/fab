01caaa7f89a9ab59e6c27857bee166d8e3b6ce04
Anirudh <icyph0x@pm.me>
2018-06-14 11:56:11 +0530

    feat: add strikethrough and running process

--- src/fab.nim
+++ src/fab.nim
@@ -28,6 +28,9 @@ proc italic*(s: string): string =
 proc under*(s: string): string =
   result = "\e[4m" & s & "\e[0m"
 
+proc strike*(s: string): string =
+  result = "\e[9m" & s & "\e[0m"
+
 
 # labels
 proc que*(s: string): string =
@@ -41,3 +44,6 @@ proc bad*(s: string): string =
 
 proc good*(s: string): string =
   result = "\e[32m[+] \e[0m" & s
+
+proc run*(s: string): string =
+  result = "\e[37m[~] \e[0m" & s
