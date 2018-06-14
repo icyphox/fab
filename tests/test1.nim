c1425192f0a4312984bc8f3a74a1fee0279da116
Anirudh <icyph0x@pm.me>
2018-06-14 11:40:31 +0530

    feat: create tests

---
+++ tests/test1.nim
@@ -0,0 +1,19 @@
+import "../src/fab"
+
+echo "Colors:"
+echo blue("this is blue")
+echo red("this is green")
+echo yellow("this is yellow")
+echo green("this is green")
+echo ""
+echo "Styles:"
+echo bold("this is bold")
+echo italic("this is italic")
+echo under("this is underlined")
+echo ""
+echo "Labels:"
+echo que("what?")
+echo info("fyi")
+echo bad("you suck")
+echo good("yay!")
+
