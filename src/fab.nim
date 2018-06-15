a1d1bb04ddb34e22526adca3403e00d1530dde01
Kaushal Modi <kaushal.modi@gmail.com>
2018-06-15 16:27:13 -0400

    Refactor; split out fabLabelEcho out of fabEcho
    Unified the API.. now the fg, sty can be applied to the text for label templates
    too.

--- src/fab.nim
+++ src/fab.nim
@@ -19,92 +19,101 @@ import terminal
 #   fgCyan,                     ## cyan
 #   fgWhite                     ## white
 
-template fabEcho*(fg: ForegroundColor; styleSet: set[Style] = {}; styled, unstyled: string = ""; newline: bool = true; brightFg = false) =
+template fabEcho*(s: string; fg: ForegroundColor; styleSet: set[Style] = {}; newline = true; brightFg = false) =
   setForeGroundColor(fg, brightFg)
-  setStyle(styleSet)
-  stdout.write(styled)
+  s.writeStyled(styleSet)
   resetAttributes()
-  if unstyled != "":
-    stdout.write(" " & unstyled)
   if newline:
     echo ""
 
-const reset = "\e[0m"
-
 # colors
 template blue*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgBlue, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgBlue, sty, nl)
 
 template yellow*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgYellow, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgYellow, sty, nl)
 
 template green*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgGreen, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgGreen, sty, nl)
 
 template red*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgRed, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgRed, sty, nl)
 
 template white*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgWhite, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgWhite, sty, nl)
 
 template purple*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgMagenta, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgMagenta, sty, nl)
 
 template cyan*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgCyan, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgCyan, sty, nl)
 
 template black*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgBlack, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgBlack, sty, nl)
 
 template grey*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgBlack, brightFg = true, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgBlack, sty, nl, brightFg = true)
 
 template orange*(s: string; sty: set[Style] = {}; nl = true) =
-  fabEcho(fgYellow, brightFg = true, styleSet = sty, styled = s, newline = nl)
+  fabEcho(s, fgYellow, sty, nl, brightFg = true)
 
 # styles
 template bold*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleBright} + sty, styled = s, newline = nl)
+  fabEcho(s, fg, {styleBright} + sty, nl)
 
+# https://github.com/nim-lang/Nim/pull/8048
 template italic*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleReverse} + sty, styled = s, newline = nl)
+  # fabEcho(s, fg, {styleItalic} + sty, nl)
+  fabEcho(s, fg, {styleUnknown} + sty, nl)
+
+template reverse*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabEcho(s, fg, {styleReverse} + sty, nl)
 
 template under*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleUnderscore} + sty, styled = s, newline = nl)
+  fabEcho(s, fg, {styleUnderscore} + sty, nl)
 
 template blink*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleBlink} + sty, styled = s, newline = nl)
+  fabEcho(s, fg, {styleBlink} + sty, nl)
 
 template dim*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleDim} + sty, styled = s, newline = nl)
+  fabEcho(s, fg, {styleDim} + sty, nl)
 
 template hidden*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
-  fabEcho(fg, styleSet = {styleHidden} + sty, styled = s, newline = nl)
+  fabEcho(s, fg, {styleHidden} + sty, nl)
 
-# proc strike*(s: string): string =
-#   result = "\e[9m" & s & reset
+# https://github.com/nim-lang/Nim/pull/8048
+# template strike*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+#   fabEcho(s, fg, {styleStrikethrough} + sty, nl)
 
 
 # labels
-template que*(s: string; nl = true) =
-  fabEcho(fgBlue, styled = "[?]", unstyled = s, newline = nl)
+template fabLabelEcho*(label: string; lFg: ForegroundColor; s: string; sFg: ForegroundColor; styleSet: set[Style] = {}; newline = true; brightFg = false) =
+  fabEcho(label, lFg, styleSet, false, brightFg)
+  stdout.write(" ")
+  fabEcho(s, sFg, styleSet, false, brightFg)
+  if newline:
+    echo ""
 
-template info*(s: string; nl = true) =
-  fabEcho(fgYellow, styled = "[!]", unstyled = s, newline = nl)
+template que*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabLabelEcho("[?]", fgBlue, s, fg, sty, nl)
 
-template bad*(s: string; nl = true) =
-  fabEcho(fgRed, styled = "[!]", unstyled = s, newline = nl)
+template info*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabLabelEcho("[!]", fgYellow, s, fg, sty, nl)
 
-template good*(s: string; nl = true) =
-  fabEcho(fgGreen, styled = "[+]", unstyled = s, newline = nl)
+template bad*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabLabelEcho("[!]", fgRed, s, fg, sty, nl)
 
-template run*(s: string; nl = true) =
-  fabEcho(fgWhite, styled = "[~]", unstyled = s, newline = nl)
+template good*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabLabelEcho("[+]", fgGreen, s, fg, sty, nl)
+
+template run*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+  fabLabelEcho("[~]", fgWhite, s, fg, sty, nl)
 
 when isMainModule:
   echo ""
   bold "COLORS"
   blue("this is blue")
+  echo "ordinary text"
   blue("this is bold blue", sty = {styleBright})
 
   blue("this is blue", nl = false) # no newline
@@ -130,12 +139,13 @@ when isMainModule:
   dim("this is yellow and dim", fg = fgYellow)
   echo "this is regular"
   bold("this is bold")
-  italic("this is italics?/reverse")
+  italic("this is italics/shows up reversed on terminals not supporting italics")
+  reverse("this is reverse")
   under("this is underlined")
   blink("this is blinking!")
   italic("this is italics and bold!", sty = {styleBright})
   dim("this is dim and reverse!", sty = {styleReverse})
-  bold("this is bold and reverse and red!!", sty = {styleReverse}, fg = fgRed)
+  bold("this is bold and reverse and red!!", fg = fgRed, sty = {styleReverse})
 
   white("before hidden text 'abcdefghijklm'", nl = false) # no newline
   hidden("abcdefghijklm", nl = false) # no newline
@@ -148,5 +158,7 @@ when isMainModule:
   que("what?")
   info("fyi")
   bad("you suck")
+  bad("everything in red bold", fg = fgRed, sty = {styleBright})
   good("yay!")
   run("hacking in progress...")
+  run("everything in bold", sty = {styleBright})
