bff9dd5a2bc150d01fe146d500883925d6da0629
Anirudh <icyph0x@pm.me>
2018-06-15 11:31:28 +0530

    refactor: reset escape code in a const

--- src/fab.nim
+++ src/fab.nim
@@ -1,58 +1,61 @@
+const reset = "\e[0m"
+
 # colors
 proc blue*(s: string): string =
-  result = "\e[34m" & s & "\e[0m"
+  result = "\e[34m" & s & reset
 
 proc yellow*(s: string): string =
-  result = "\e[33m" & s & "\e[0m"
+  result = "\e[33m" & s & reset
 
 proc green*(s: string): string =
-  result = "\e[32m" & s & "\e[0m"
+  result = "\e[32m" & s & reset
 
 proc red*(s: string): string =
-  result = "\e[31m" & s & "\e[0m"
+  result = "\e[31m" & s & reset
 
 proc white*(s: string): string =
-  result = "\e[97m" & s & "\e[0m"
+  result = "\e[97m" & s & reset
 
 # TODO: orange is bold yellow lol
 proc orange*(s: string): string =
-  result = "\e[93;1m" & s & "\e[0m"
+  result = "\e[93;1m" & s & reset
 
 proc purple*(s: string): string =
-  result = "\e[35m" & s & "\e[0m"
+  result = "\e[35m" & s & reset
 
 proc black*(s: string): string =
-  result = "\e[30;1m" & s & "\e[0m"
+  result = "\e[30;1m" & s & reset
 
 proc cyan*(s: string): string =
-  result = "\e[36m" & s & "\e[0m"
+  result = "\e[36m" & s & reset
 
 # styles
 proc bold*(s: string): string =
-  result = "\e[1m" & s & "\e[0m"
+  result = "\e[1m" & s & reset
 
 proc italic*(s: string): string =
-  result = "\e[3m" & s & "\e[0m"
+  result = "\e[3m" & s & reset
 
 proc under*(s: string): string =
-  result = "\e[4m" & s & "\e[0m"
+  result = "\e[4m" & s & reset
 
 proc strike*(s: string): string =
-  result = "\e[9m" & s & "\e[0m"
+  result = "\e[9m" & s & reset
 
 
 # labels
 proc que*(s: string): string =
-  result = "\e[34m[?] \e[0m" & s
+  result = "\e[34m[?]" & reset & s
 
 proc info*(s: string): string =
-  result = "\e[33m[*] \e[0m" & s
+  result = "\e[33m[*]" & reset & s
 
 proc bad*(s: string): string =
-  result = "\e[31m[!] \e[0m" & s
+  result = "\e[31m[!]" & reset & s
 
 proc good*(s: string): string =
-  result = "\e[32m[+] \e[0m" & s
+  result = "\e[32m[+]" & reset & s
 
 proc run*(s: string): string =
-  result = "\e[97m[~] \e[0m" & s
+  result = "\e[97m[~]" & reset & s
+
