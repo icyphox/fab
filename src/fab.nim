2e08f0f9a838d493edd1a1823a0d12b4aba8a295
Kaushal Modi <kaushal.modi@gmail.com>
2018-06-20 09:50:41 -0400

    Prevent excessive use of templates
    Use procs instead.
    
    https://forum.nim-lang.org/t/3948#24634

--- src/fab.nim
+++ src/fab.nim
@@ -1,24 +1,5 @@
 import terminal
 
-# Style = enum
-#   styleBright = 1,            ## bright text
-#   styleDim,                   ## dim text
-#   styleUnknown,               ## unknown
-#   styleUnderscore = 4,        ## underscored text
-#   styleBlink,                 ## blinking/bold text
-#   styleReverse = 7,           ## unknown
-#   styleHidden                 ## hidden text
-
-# ForegroundColor = enum
-#   fgBlack = 30,               ## black
-#   fgRed,                      ## red
-#   fgGreen,                    ## green
-#   fgYellow,                   ## yellow
-#   fgBlue,                     ## blue
-#   fgMagenta,                  ## magenta
-#   fgCyan,                     ## cyan
-#   fgWhite                     ## white
-
 template fabEcho*(s: string; fg: ForegroundColor; styleSet: set[Style] = {}; newline = true; brightFg = false) =
   setForeGroundColor(fg, brightFg)
   s.writeStyled(styleSet)
@@ -27,58 +8,58 @@ template fabEcho*(s: string; fg: ForegroundColor; styleSet: set[Style] = {}; new
     echo ""
 
 # colors
-template blue*(s: string; sty: set[Style] = {}; nl = true) =
+proc blue*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgBlue, sty, nl)
 
-template yellow*(s: string; sty: set[Style] = {}; nl = true) =
+proc yellow*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgYellow, sty, nl)
 
-template green*(s: string; sty: set[Style] = {}; nl = true) =
+proc green*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgGreen, sty, nl)
 
-template red*(s: string; sty: set[Style] = {}; nl = true) =
+proc red*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgRed, sty, nl)
 
-template white*(s: string; sty: set[Style] = {}; nl = true) =
+proc white*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgWhite, sty, nl)
 
-template purple*(s: string; sty: set[Style] = {}; nl = true) =
+proc purple*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgMagenta, sty, nl)
 
-template cyan*(s: string; sty: set[Style] = {}; nl = true) =
+proc cyan*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgCyan, sty, nl)
 
-template black*(s: string; sty: set[Style] = {}; nl = true) =
+proc black*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgBlack, sty, nl)
 
-template grey*(s: string; sty: set[Style] = {}; nl = true) =
+proc grey*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgBlack, sty, nl, brightFg = true)
 
-template orange*(s: string; sty: set[Style] = {}; nl = true) =
+proc orange*(s: string; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fgYellow, sty, nl, brightFg = true)
 
 # styles
-template bold*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc bold*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleBright} + sty, nl)
 
 # https://github.com/nim-lang/Nim/pull/8048
-template italic*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc italic*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   # fabEcho(s, fg, {styleItalic} + sty, nl)
   fabEcho(s, fg, {styleUnknown} + sty, nl)
 
-template reverse*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc reverse*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleReverse} + sty, nl)
 
-template under*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc under*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleUnderscore} + sty, nl)
 
-template blink*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc blink*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleBlink} + sty, nl)
 
-template dim*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc dim*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleDim} + sty, nl)
 
-template hidden*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc hidden*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabEcho(s, fg, {styleHidden} + sty, nl)
 
 # https://github.com/nim-lang/Nim/pull/8048
@@ -87,26 +68,26 @@ template hidden*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {};
 
 
 # labels
-template fabLabelEcho*(label: string; lFg: ForegroundColor; s: string; sFg: ForegroundColor; styleSet: set[Style] = {}; newline = true; brightFg = false) =
+proc fabLabelEcho*(label: string; lFg: ForegroundColor; s: string; sFg: ForegroundColor; styleSet: set[Style] = {}; newline = true; brightFg = false) =
   fabEcho(label, lFg, styleSet, false, brightFg)
   stdout.write(" ")
   fabEcho(s, sFg, styleSet, false, brightFg)
   if newline:
     echo ""
 
-template que*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc que*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabLabelEcho("[?]", fgBlue, s, fg, sty, nl)
 
-template info*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc info*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabLabelEcho("[!]", fgYellow, s, fg, sty, nl)
 
-template bad*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc bad*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabLabelEcho("[!]", fgRed, s, fg, sty, nl)
 
-template good*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc good*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabLabelEcho("[+]", fgGreen, s, fg, sty, nl)
 
-template run*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
+proc run*(s: string; fg: ForegroundColor = fgWhite; sty: set[Style] = {}; nl = true) =
   fabLabelEcho("[~]", fgWhite, s, fg, sty, nl)
 
 when isMainModule:
